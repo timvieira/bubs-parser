70750809822b2521efc7e70d1eb84ad81b35180d
dunlopa <devnull@localhost>
2009-05-08 19:53:21 +0000

    Added PairwiseDistanceHeap interface for efficient access to pairwise distances (such as for iterative pairwise alignment) and two implementations which pack both indices and the distance into intrinsic longs (using fastutil's type-specific heap implementation).

---
+++ java/src/edu/ohsu/cslu/datastructs/BaseLongPairwiseDistanceHeap.java
@@ -0,0 +1,48 @@
+package edu.ohsu.cslu.datastructs;
+
+import it.unimi.dsi.fastutil.longs.LongHeapPriorityQueue;
+
+/**
+ * Implements {@link PairwiseDistanceHeap}, packing all three fields of each element (distance and two
+ * indices) into a single long for memory efficiency.
+ * 
+ * @author Aaron Dunlop
+ * @since May 8, 2009
+ * 
+ * @version $Revision$ $Date$ $Author$
+ */
+public abstract class BaseLongPairwiseDistanceHeap implements PairwiseDistanceHeap
+{
+    protected final LongHeapPriorityQueue priorityQueue = new LongHeapPriorityQueue();
+
+    protected void checkRanges(int index1, int index2, float distance)
+    {
+        if (distance < 0)
+        {
+            throw new IllegalArgumentException("Distance cannot be less than 0");
+        }
+
+        if (Float.isInfinite(distance) || Float.isNaN(distance))
+        {
+            throw new IllegalArgumentException("Distance cannot be infinite or NaN");
+        }
+
+        if (index1 < 0 || index2 < 0)
+        {
+            throw new IllegalArgumentException("Indices < 0 are not supported");
+        }
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        return priorityQueue.isEmpty();
+    }
+
+    @Override
+    public int size()
+    {
+        return priorityQueue.size();
+    }
+
+}
