b2f48dba4b5ce8c6bc80c008c8464279316e914d
dunlopa <devnull@localhost>
2009-12-24 04:59:41 +0000

    Documentation updates, added parser test launch configurations to debug menu.

--- java/src/edu/ohsu/cslu/parser/ExhaustiveChartParserTestCase.java
+++ java/src/edu/ohsu/cslu/parser/ExhaustiveChartParserTestCase.java
@@ -35,16 +35,34 @@ import edu.ohsu.cslu.tests.SharedNlpTests;
 @RunWith(FilteredRunner.class)
 public abstract class ExhaustiveChartParserTestCase {
 
+    // Grammar file paths, relative to unit test data root directory
     private final static String PCFG_FILE = "grammars/f2-21-R2-p1-unk.pcfg";
     private final static String LEX_FILE = "grammars/f2-21-R2-p1-unk.lex";
 
+    /** Grammar induced from WSJ sections 2-21 */
     protected static Grammar f2_21_grammar;
+
+    /** WSJ section 24 sentences 1-20 */
     private static ArrayList<String[]> sentences = new ArrayList<String[]>();
+
+    /** The parser under test */
     private MaximumLikelihoodParser parser;
 
+    /**
+     * Creates the appropriate parser for each test class.
+     * 
+     * @param grammar
+     *            The grammar to use when parsing
+     * @param chartTraversalType
+     *            The chart traversal order
+     * @return Parser instance
+     */
     protected abstract MaximumLikelihoodParser createParser(Grammar grammar,
             ChartTraversalType chartTraversalType);
 
+    /**
+     * @return the grammar class appropriate for the parser under test
+     */
     protected abstract Class<? extends Grammar> grammarClass();
 
     private Grammar createGrammar(final Reader grammarReader, final Reader lexiconReader) throws Exception {
@@ -52,6 +70,13 @@ public abstract class ExhaustiveChartParserTestCase {
             new Object[] { grammarReader, lexiconReader });
     }
 
+    /**
+     * Reads in the first 20 sentences of WSJ section 24. Run once for the class, prior to execution of the
+     * first test method.
+     * 
+     * @throws Exception
+     *             if unable to read
+     */
     @BeforeClass
     public static void suiteSetUp() throws Exception {
         // Read test sentences
@@ -69,6 +94,12 @@ public abstract class ExhaustiveChartParserTestCase {
         }
     }
 
+    /**
+     * Constructs the grammar (if necessary) and a new parser instance. Run prior to each test method.
+     * 
+     * @throws Exception
+     *             if unable to construct grammar or parser.
+     */
     @Before
     public void setUp() throws Exception {
         if (f2_21_grammar == null || f2_21_grammar.getClass() != grammarClass()) {
@@ -80,6 +111,12 @@ public abstract class ExhaustiveChartParserTestCase {
         parser = createParser(f2_21_grammar, ChartTraversalType.LeftRightBottomTopTraversal);
     }
 
+    /**
+     * Tests parsing with a _very_ simple grammar.
+     * 
+     * @throws Exception
+     *             if something bad happens
+     */
     @Test
     public void testSimpleGrammar() throws Exception {
         final String sentence = "systems analyst arbitration chef";
@@ -111,65 +148,72 @@ public abstract class ExhaustiveChartParserTestCase {
 
     @Test
     public void testSentence1() throws Exception {
-        parseSentence(0);
+        parseTreebankSentence(0);
     }
 
     @Test
     public void testSentence2() throws Exception {
-        parseSentence(1);
+        parseTreebankSentence(1);
     }
 
     @Test
     public void testSentence3() throws Exception {
-        parseSentence(2);
+        parseTreebankSentence(2);
     }
 
     @Test
     public void testSentence4() throws Exception {
-        parseSentence(3);
+        parseTreebankSentence(3);
     }
 
     @Test
     public void testSentence5() throws Exception {
-        parseSentence(4);
+        parseTreebankSentence(4);
     }
 
     @Test
     public void testSentence6() throws Exception {
-        parseSentence(5);
+        parseTreebankSentence(5);
     }
 
     @Test
     public void testSentence7() throws Exception {
-        parseSentence(6);
+        parseTreebankSentence(6);
     }
 
     @Test
     public void testSentence8() throws Exception {
-        parseSentence(7);
+        parseTreebankSentence(7);
     }
 
     @Test
     public void testSentence9() throws Exception {
-        parseSentence(8);
+        parseTreebankSentence(8);
     }
 
     @Test
     public void testSentence10() throws Exception {
-        parseSentence(9);
+        parseTreebankSentence(9);
     }
 
+    /**
+     * Profiles parsing sentences 11-20 of WSJ section 24. This method must be overridden (calling
+     * {@link #internalProfileSentences11Through20()}) in each subclass, simply to allow re-annotating the
+     * {@link PerformanceTest} annotation with the expected performance for that implementation.
+     * 
+     * @throws Exception
+     */
     @Test
-    @PerformanceTest( { "mbp", "9495" })
+    @PerformanceTest( { "mbp", "0" })
     public abstract void profileSentences11Through20() throws Exception;
 
     protected void internalProfileSentences11Through20() throws Exception {
         for (int i = 10; i < 20; i++) {
-            parseSentence(i);
+            parseTreebankSentence(i);
         }
     }
 
-    protected void parseSentence(final int index) throws Exception {
+    protected void parseTreebankSentence(final int index) throws Exception {
         final ParseTree bestParseTree = parser.findMLParse(sentences.get(index)[0]);
         assertEquals(sentences.get(index)[1], bestParseTree.toString());
     }
