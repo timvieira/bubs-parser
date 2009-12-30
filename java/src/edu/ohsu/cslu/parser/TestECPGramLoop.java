cfc52d420694bd9947e851c1bf208a5026050306
dunlopa <devnull@localhost>
2009-12-30 05:47:13 +0000

    Updated profiling timings for MacBook Pro

--- java/src/edu/ohsu/cslu/parser/TestECPGramLoop.java
+++ java/src/edu/ohsu/cslu/parser/TestECPGramLoop.java
@@ -29,7 +29,7 @@ public class TestECPGramLoop extends ExhaustiveChartParserTestCase {
 
     @Override
     @Test
-    @PerformanceTest( { "mbp", "20759", "d820", "20297" })
+    @PerformanceTest( { "mbp", "17799", "d820", "20297" })
     public void profileSentences11Through20() throws Exception {
         internalProfileSentences11Through20();
     }
