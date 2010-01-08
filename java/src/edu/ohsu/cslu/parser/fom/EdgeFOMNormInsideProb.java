646a4e6f2d49a80740e288c3039446194694442a
Nate <devnull@localhost>
2010-01-08 02:11:57 +0000

    Had to make the fugeFactor static for now ... kind of a hack since we don't have access to ParserOptions anymore.

--- java/src/edu/ohsu/cslu/parser/fom/EdgeFOMNormInsideProb.java
+++ java/src/edu/ohsu/cslu/parser/fom/EdgeFOMNormInsideProb.java
@@ -3,26 +3,27 @@ package edu.ohsu.cslu.parser.fom;
 import edu.ohsu.cslu.parser.ArrayChartCell;
 import edu.ohsu.cslu.parser.ChartEdgeWithFOM;
 import edu.ohsu.cslu.parser.ChartParser;
+import edu.ohsu.cslu.parser.ParserDriver;
 
 public class EdgeFOMNormInsideProb extends EdgeFOM {
 
-	@Override
-	public float calcFOM(final ChartEdgeWithFOM edge, final ChartParser parser) {
-		// if (edge.p.isUnaryProd()) {
-		// return edge.insideProb;
-		// }
-		ArrayChartCell rightCell = edge.rightCell;
-		if (rightCell == null) {
-			rightCell = edge.leftCell;
-		}
+    @Override
+    public float calcFOM(final ChartEdgeWithFOM edge, final ChartParser parser) {
+        // if (edge.p.isUnaryProd()) {
+        // return edge.insideProb;
+        // }
+        ArrayChartCell rightCell = edge.rightCell;
+        if (rightCell == null) {
+            rightCell = edge.leftCell;
+        }
 
-		final int spanLength = rightCell.end - edge.leftCell.start;
-		// return edge.insideProb / spanLength;
-		// return edge.insideProb - (float) (Math.log(spanLength) * 0.01);
-		// return edge.insideProb - (float) Math.pow(0.7, spanLength);
-		return edge.insideProb + parser.opts.fudgeFactor * spanLength;
-		// for f24 sent 1 and R2 grammar, !7 seems fast and ~5 seems accurate
-		// Should probably come up with a formulate that is dependent on the grammar
-	}
+        final int spanLength = rightCell.end - edge.leftCell.start;
+        // return edge.insideProb / spanLength;
+        // return edge.insideProb - (float) (Math.log(spanLength) * 0.01);
+        // return edge.insideProb - (float) Math.pow(0.7, spanLength);
+        return edge.insideProb + ParserDriver.fudgeFactor * spanLength;
+        // for f24 sent 1 and R2 grammar, !7 seems fast and ~5 seems accurate
+        // Should probably come up with a formulate that is dependent on the grammar
+    }
 
 }
