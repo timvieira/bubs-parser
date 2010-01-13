c548aaf938c7fe166ff448fbb885d8be7f8d0ed5
Nate <devnull@localhost>
2010-01-13 18:36:08 +0000

    Renamed class files since they are all in the FOM package

--- java/src/edu/ohsu/cslu/parser/fom/EdgeFOM.java
+++ java/src/edu/ohsu/cslu/parser/fom/EdgeFOM.java
@@ -10,41 +10,43 @@ import edu.ohsu.cslu.parser.util.Log;
 
 public abstract class EdgeFOM {
 
-	static public enum EdgeFOMType {
-		Inside, NormalizedInside, BoundaryNgram
-	}
+    static public enum EdgeFOMType {
+        Inside, NormalizedInside, BoundaryInOut, WeightedFeatures
+    }
 
-	public abstract float calcFOM(ChartEdgeWithFOM edge, ChartParser parser);
+    public abstract float calcFOM(ChartEdgeWithFOM edge, ChartParser parser);
 
-	public static EdgeFOM create(final EdgeFOMType type, final ArrayGrammar grammar) {
-		switch (type) {
-		case Inside:
-			return new EdgeFOMInsideProb();
-		case NormalizedInside:
-			return new EdgeFOMNormInsideProb();
-		case BoundaryNgram:
-			return new EdgeFOMBoundaryNgram(grammar);
-		default:
-			Log.info(0, "ERROR: EdgeFOM " + type + " not supported.");
-			System.exit(1);
-			return null;
-		}
-	}
+    public static EdgeFOM create(final EdgeFOMType type, final ArrayGrammar grammar) {
+        switch (type) {
+        case Inside:
+            return new InsideProb();
+        case NormalizedInside:
+            return new NormalizedInsideProb();
+        case BoundaryInOut:
+            return new BoundaryInOut(grammar);
+        case WeightedFeatures:
+            return new WeightedFeatures(grammar);
+        default:
+            Log.info(0, "ERROR: EdgeFOM " + type + " not supported.");
+            System.exit(1);
+            return null;
+        }
+    }
 
-	public void init(final ChartParser parser) {
-		// default is to do nothing
-	}
+    public void init(final ChartParser parser) {
+        // default is to do nothing
+    }
 
-	public void train(final BufferedReader inStream) throws Exception {
-		throw new Exception("Not implemented.");
-	}
+    public void train(final BufferedReader inStream) throws Exception {
+        throw new Exception("Not implemented.");
+    }
 
-	public void readModel(final BufferedReader inStream) throws Exception {
-		throw new Exception("Not implemented.");
-	}
+    public void readModel(final BufferedReader inStream) throws Exception {
+        throw new Exception("Not implemented.");
+    }
 
-	public void writeModel(final BufferedWriter outStream) throws Exception {
-		throw new Exception("Not implemented.");
-	}
+    public void writeModel(final BufferedWriter outStream) throws Exception {
+        throw new Exception("Not implemented.");
+    }
 
 }
