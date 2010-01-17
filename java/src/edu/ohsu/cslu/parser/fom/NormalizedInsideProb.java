a4c1c115a77ffd077c1067a5218c3bb37581fb3a
Nate <devnull@localhost>
2010-01-17 17:19:32 +0000

    cleaned up code

--- java/src/edu/ohsu/cslu/parser/fom/NormalizedInsideProb.java
+++ java/src/edu/ohsu/cslu/parser/fom/NormalizedInsideProb.java
@@ -10,12 +10,7 @@ public class NormalizedInsideProb extends EdgeFOM {
     public float calcFOM(final ChartEdgeWithFOM edge, final ChartParser parser) {
 
         final int spanLength = edge.end() - edge.start();
-        // return edge.insideProb / spanLength;
-        // return edge.insideProb - (float) (Math.log(spanLength) * 0.01);
-        // return edge.insideProb - (float) Math.pow(0.7, spanLength);
-        return edge.insideProb + ParserDriver.fudgeFactor * spanLength;
-        // for f24 sent 1 and R2 grammar, !7 seems fast and ~5 seems accurate
-        // Should probably come up with a formulate that is dependent on the grammar
+        return edge.insideProb + spanLength * ParserDriver.fudgeFactor;
     }
 
 }
