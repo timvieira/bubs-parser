60252e0932f6d55244120796750fd10e9251b4c7
Nate <devnull@localhost>
2010-01-19 18:55:40 +0000

    Removed need to pass Parser pointer around when calculating FOM

--- java/src/edu/ohsu/cslu/parser/fom/InsideProb.java
+++ java/src/edu/ohsu/cslu/parser/fom/InsideProb.java
@@ -1,12 +1,11 @@
 package edu.ohsu.cslu.parser.fom;
 
 import edu.ohsu.cslu.parser.ChartEdgeWithFOM;
-import edu.ohsu.cslu.parser.ChartParser;
 
 public class InsideProb extends EdgeFOM {
 
-	@Override
-	public float calcFOM(final ChartEdgeWithFOM edge, final ChartParser parser) {
-		return edge.insideProb;
-	}
+    @Override
+    public float calcFOM(final ChartEdgeWithFOM edge) {
+        return edge.insideProb;
+    }
 }
--- java/src/edu/ohsu/cslu/parser/fom/NormalizedInsideProb.java
+++ java/src/edu/ohsu/cslu/parser/fom/NormalizedInsideProb.java
@@ -1,13 +1,12 @@
 package edu.ohsu.cslu.parser.fom;
 
 import edu.ohsu.cslu.parser.ChartEdgeWithFOM;
-import edu.ohsu.cslu.parser.ChartParser;
 import edu.ohsu.cslu.parser.ParserDriver;
 
 public class NormalizedInsideProb extends EdgeFOM {
 
     @Override
-    public float calcFOM(final ChartEdgeWithFOM edge, final ChartParser parser) {
+    public float calcFOM(final ChartEdgeWithFOM edge) {
 
         final int spanLength = edge.end() - edge.start();
         return edge.insideProb + spanLength * ParserDriver.fudgeFactor;
