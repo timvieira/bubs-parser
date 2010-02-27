ddfb98585d4316e8d45088cdd8d5e0415488004b
Nate <devnull@localhost>
2010-02-27 18:25:39 +0000

    adding sub package for tests

---
+++ java/src/edu/ohsu/cslu/grammar/test/TestHashSparseMatrixGrammar.java
@@ -0,0 +1,10 @@
+package edu.ohsu.cslu.grammar.test;
+
+//public class TestHashSparseMatrixGrammar extends GrammarTestCase {
+//
+//    @Override
+//    protected Class<? extends Grammar> grammarClass() {
+//        return HashSparseMatrixGrammar.class;
+//    }
+//
+// }
---
+++ java/src/edu/ohsu/cslu/grammar/test/TestPackedSparseMatrixGrammar.java
@@ -0,0 +1,10 @@
+package edu.ohsu.cslu.grammar.test;
+
+//public class TestPackedSparseMatrixGrammar extends GrammarTestCase {
+//
+//    @Override
+//    protected Class<? extends BaseGrammar> grammarClass() {
+//        return PackedSparseMatrixGrammar.class;
+//    }
+//
+// }
