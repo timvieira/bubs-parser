797ec7e4088dc2222292821ead12ea666ee7b08c
Nate <devnull@localhost>
2010-03-04 21:24:41 +0000

    Removed because not useful

--- java/src/edu/ohsu/cslu/parser/MaximumLikelihoodParser.java
+++
@@ -1,8 +0,0 @@
-package edu.ohsu.cslu.parser;
-
-import edu.ohsu.cslu.parser.util.ParseTree;
-
-public interface MaximumLikelihoodParser {
-
-    public ParseTree findMLParse(String sentence) throws Exception;
-}
