5bfdbbb3cd6518b3ad789924b5e69d32d0d1946a
dunlopa <devnull@localhost>
2010-04-06 00:19:32 +0000

    Added recognition matrix histogram output to SummarizeGrammar. Added additional-root-classes parameter to package-jar target and used it when building summarize-grammar tool.

--- java/src/edu/ohsu/cslu/grammar/SummarizeGrammar.java
+++ java/src/edu/ohsu/cslu/grammar/SummarizeGrammar.java
@@ -1,11 +1,15 @@
 package edu.ohsu.cslu.grammar;
 
+import it.unimi.dsi.fastutil.ints.Int2IntMap;
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Arrays;
 import java.util.zip.GZIPInputStream;
 
 import org.kohsuke.args4j.Argument;
@@ -19,6 +23,14 @@ public class SummarizeGrammar extends BaseCommandlineTool {
     @Option(name = "-c", aliases = { "--class" }, metaVar = "class name", usage = "Grammar class")
     private String grammarClass = Grammar.class.getName();
 
+    // TODO Switch to a mode enum
+
+    @Option(name = "-rm", aliases = { "--recognition-matrix" }, usage = "Print recognition matrix (populated pairs in |V| x |V| matrix)")
+    private boolean recognitionMatrix;
+
+    @Option(name = "-rmh", aliases = { "--recognition-matrix-histogram" }, usage = "Print recognition matrix histogram (counts of occurrences in the |V| x |V| recognition matrix)")
+    private boolean recognitionMatrixHistogram;
+
     @Argument(index = 0, required = true, metaVar = "prefix", usage = "Grammar file prefix")
     private String prefix;
 
@@ -29,6 +41,10 @@ public class SummarizeGrammar extends BaseCommandlineTool {
     @SuppressWarnings("unchecked")
     @Override
     protected void run() throws Exception {
+        if (recognitionMatrix || recognitionMatrixHistogram) {
+            grammarClass = CsrSparseMatrixGrammar.class.getName();
+        }
+
         final Class<Grammar> gc = (Class<Grammar>) Class.forName(grammarClass);
         final java.lang.reflect.Constructor<Grammar> c = gc.getConstructor(Reader.class, Reader.class, GrammarFormatType.class);
 
@@ -52,7 +68,34 @@ public class SummarizeGrammar extends BaseCommandlineTool {
         }
 
         final Grammar g = c.newInstance(pcfgReader, lexiconReader, GrammarFormatType.CSLU);
-        System.out.println(g.getStats());
+
+        if (recognitionMatrix) {
+            final SparseMatrixGrammar spmg = (SparseMatrixGrammar) g;
+            System.out.print(spmg.recognitionMatrix());
+        } else if (recognitionMatrixHistogram) {
+            final CsrSparseMatrixGrammar spmg = (CsrSparseMatrixGrammar) g;
+            final Int2IntMap map = new Int2IntOpenHashMap();
+
+            for (final int children : spmg.binaryRuleMatrixColumnIndices()) {
+                map.put(children, map.get(children) + 1);
+            }
+
+            final int[] counts = map.values().toIntArray();
+            Arrays.sort(counts);
+
+            int prev = counts[0], prevCount = 0;
+            for (int i = 0; i < counts.length; i++) {
+                if (counts[i] != prev) {
+                    System.out.println(prev + ":" + prevCount);
+                    prev = counts[i];
+                    prevCount = 0;
+                }
+                prevCount++;
+            }
+            System.out.println(prev + ":" + prevCount);
+        } else {
+            System.out.println(g.getStats());
+        }
     }
 
 }
