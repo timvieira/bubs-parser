09e72788f2cc55b7f24608b8d7da91720b57b216
dunlopa <devnull@localhost>
2010-08-26 23:26:11 +0000

    Moved SummaryGrammar into SummarizeGrammar tool (since that's its only consumer)

--- java/src/edu/ohsu/cslu/grammar/SummarizeGrammar.java
+++ java/src/edu/ohsu/cslu/grammar/SummarizeGrammar.java
@@ -1,5 +1,8 @@
 package edu.ohsu.cslu.grammar;
 
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -51,5 +54,30 @@ public class SummarizeGrammar extends BaseCommandlineTool {
 
         final SummaryGrammar grammar = new SummaryGrammar(pcfgReader, lexiconReader, grammarFormat);
         System.out.print(grammar.getStats());
+    }
+
+    private class SummaryGrammar extends SortedGrammar {
+
+        private final int V_l, V_r;
+
+        public SummaryGrammar(final Reader grammarFile, final Reader lexiconFile,
+                final GrammarFormatType grammarFormat) throws Exception {
+            super(grammarFile, lexiconFile, grammarFormat);
+
+            final IntSet vlSet = new IntOpenHashSet();
+            final IntSet vrSet = new IntOpenHashSet();
+            for (final Production p : binaryProductions) {
+                vlSet.add(p.leftChild);
+                vrSet.add(p.rightChild);
+            }
+            V_l = vlSet.size();
+            V_r = vrSet.size();
+        }
+
+        @Override
+        public String getStats() {
+            return super.getStats() + "V_l: " + V_l + '\n' + "V_r: " + V_r + '\n';
+        }
+
     }
 }
