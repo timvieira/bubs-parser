1c7934d0015b19835b413d18b0c146bc4e41d5fe
Nate <devnull@localhost>
2010-09-30 18:00:22 +0000

    updates to Grammar and ProjectedGrammar (for latent variable marginalization) but the ProjectedGrammar still doesn't work

--- java/src/edu/ohsu/cslu/grammar/ProjectedGrammar.java
+++ java/src/edu/ohsu/cslu/grammar/ProjectedGrammar.java
@@ -1,21 +1,28 @@
 package edu.ohsu.cslu.grammar;
 
+import java.util.HashMap;
+
 public class ProjectedGrammar extends Grammar {
 
     Grammar parentGrammar;
     int ntProjection[];
 
     public ProjectedGrammar(final Grammar parentGrammar) {
-        super(parentGrammar);
+        // NOTE: This class DOES NOT WORK!!! Need projected productions
+        super(parentGrammar); // TODO: fix this
 
         this.parentGrammar = parentGrammar;
         this.grammarFormat = parentGrammar.grammarFormat;
+        this.lexSet = parentGrammar.lexSet;
+        this.ntProjection = new int[parentGrammar.numNonTerms()];
 
         // create mapping from parent grammar non-terms to the non-terms in this grammar
         ntProjection = new int[parentGrammar.numNonTerms()];
         for (int i = 0; i < parentGrammar.numNonTerms(); i++) {
             final String parentNonTermString = parentGrammar.mapNonterminal(i);
-            ntProjection[i] = nonTermSet.addSymbol(projectNonTermString(parentNonTermString));
+            final String projNTStr = projectNonTermString(parentNonTermString);
+            // System.out.println(parentNonTermString + " => " + projNTStr);
+            ntProjection[i] = nonTermSet.addSymbol(projNTStr);
 
             if (i == parentGrammar.nullSymbol) {
                 this.nullSymbol = ntProjection[i];
@@ -24,7 +31,49 @@ public class ProjectedGrammar extends Grammar {
                 this.startSymbol = ntProjection[i];
             }
         }
+
+        // * O(1) access from a parentGrammar.prod => this.prod
+        // Using a HashMap because I can't get the HashSet does not have a get() method
+        // and I want to get a canonical production for all possible projection mappings
+        // since we will be accumulating scores given this prod.
+        final HashMap<Production, Production> prods = new HashMap<Production, Production>();
+        for (final Production p : parentGrammar.binaryProductions) {
+            final int A = projectNonTerm(p.parent);
+            final int B = projectNonTerm(p.leftChild);
+            final int C = projectNonTerm(p.rightChild);
+            final Production possProd = new Production(A, B, C, Float.NEGATIVE_INFINITY);
+            Production grammarProd = prods.get(possProd);
+            if (grammarProd == null) {
+                prods.put(possProd, possProd);
+                grammarProd = possProd;
+            }
+            // p.projProd = grammarProd;
+        }
+
+        prods.clear();
+        for (final Production p : parentGrammar.unaryProductions) {
+            final int A = projectNonTerm(p.parent);
+            final int B = projectNonTerm(p.child());
+            final Production possProd = new Production(A, B, Float.NEGATIVE_INFINITY, false);
+            Production grammarProd = prods.get(possProd);
+            if (grammarProd == null) {
+                prods.put(possProd, possProd);
+                grammarProd = possProd;
+            }
+            // p.projProd = grammarProd;
+        }
     }
+
+    // private Production getProduction(final Production p, final List<Production>[] hashSet) {
+    // final int v = ((int) Math.pow(p.parent, p.leftChild) + p.rightChild) % hashSet.length;
+    // if (hashSet[v] == null)
+    // return null;
+    // for (final Production grammarProd : hashSet[v]) {
+    // if (grammarProd.equals(p))
+    // return grammarProd;
+    // }
+    // return null;
+    // }
 
     public int projectNonTerm(final int parentGrammarNT) {
         return ntProjection[parentGrammarNT];
