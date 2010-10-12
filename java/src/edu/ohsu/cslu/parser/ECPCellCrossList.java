cdd644bff14f4f13e38b81bd9e57c0443d8181b8
Nate <devnull@localhost>
2010-10-12 19:06:27 +0000

    * added possibility of Cell Constraints and Unary Constraints

--- java/src/edu/ohsu/cslu/parser/ECPCellCrossList.java
+++ java/src/edu/ohsu/cslu/parser/ECPCellCrossList.java
@@ -16,23 +16,30 @@ public class ECPCellCrossList extends ChartParser<LeftListGrammar, CellChart> {
         final HashSetChartCell cell = chart.getCell(start, end);
         float leftInside, rightInside;
 
+        final boolean onlyFactored = (hasCellConstraints && cellConstraints.factoredParentsOnly(start, end));
+        // System.out.println("[" + start + "," + end + "] " + onlyFactored);
+
         for (int mid = start + 1; mid <= end - 1; mid++) { // mid point
             final HashSetChartCell leftCell = chart.getCell(start, mid);
             final HashSetChartCell rightCell = chart.getCell(mid, end);
             for (final int leftNT : leftCell.getLeftChildNTs()) {
                 leftInside = leftCell.getInside(leftNT);
                 for (final Production p : grammar.getBinaryProductionsWithLeftChild(leftNT)) {
-                    rightInside = rightCell.getInside(p.rightChild);
-                    if (rightInside > Float.NEGATIVE_INFINITY) {
-                        cell.updateInside(p, leftCell, rightCell, p.prob + leftInside + rightInside);
+                    if (!onlyFactored || grammar.getNonterminal(p.parent).isFactored()) {
+                        rightInside = rightCell.getInside(p.rightChild);
+                        if (rightInside > Float.NEGATIVE_INFINITY) {
+                            cell.updateInside(p, leftCell, rightCell, p.prob + leftInside + rightInside);
+                        }
                     }
                 }
             }
         }
 
-        for (final int childNT : cell.getNtArray()) {
-            for (final Production p : grammar.getUnaryProductionsWithChild(childNT)) {
-                cell.updateInside(p, p.prob + cell.getInside(childNT));
+        if ((start - end) > 1 || !hasCellConstraints || cellConstraints.unaryOpen(start, end)) {
+            for (final int childNT : cell.getNtArray()) {
+                for (final Production p : grammar.getUnaryProductionsWithChild(childNT)) {
+                    cell.updateInside(p, p.prob + cell.getInside(childNT));
+                }
             }
         }
     }
