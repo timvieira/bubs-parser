9403eef505c7ba32e8072787adb54d789e5ec670
Nate <devnull@localhost>
2011-01-10 18:41:21 +0000

    Beam-width prediction changes for ACL 2011

--- java/src/edu/ohsu/cslu/parser/ECPCellCrossList.java
+++ java/src/edu/ohsu/cslu/parser/ECPCellCrossList.java
@@ -16,7 +16,8 @@ public class ECPCellCrossList extends ChartParser<LeftListGrammar, CellChart> {
         final HashSetChartCell cell = chart.getCell(start, end);
         float leftInside, rightInside;
 
-        final boolean onlyFactored = (hasCellConstraints && cellConstraints.factoredParentsOnly(start, end));
+        // final boolean onlyFactored = (hasCellConstraints && cellConstraints.factoredParentsOnly(start, end));
+        final boolean onlyFactored = cellSelector.isOpenOnlyFactored(start, end);
 
         for (int mid = start + 1; mid <= end - 1; mid++) { // mid point
             final HashSetChartCell leftCell = chart.getCell(start, mid);
@@ -28,16 +29,19 @@ public class ECPCellCrossList extends ChartParser<LeftListGrammar, CellChart> {
                         rightInside = rightCell.getInside(p.rightChild);
                         if (rightInside > Float.NEGATIVE_INFINITY) {
                             cell.updateInside(p, leftCell, rightCell, p.prob + leftInside + rightInside);
+                            currentInput.totalConsidered++;
                         }
                     }
                 }
             }
         }
 
-        if (!hasCellConstraints || cellConstraints.unaryOpen(start, end)) {
+        // if (!hasCellConstraints || cellConstraints.unaryOpen(start, end)) {
+        if (cellSelector.isOpenUnary(start, end)) {
             for (final int childNT : cell.getNtArray()) {
                 for (final Production p : grammar.getUnaryProductionsWithChild(childNT)) {
                     cell.updateInside(p, p.prob + cell.getInside(childNT));
+                    currentInput.totalConsidered++;
                 }
             }
         }
