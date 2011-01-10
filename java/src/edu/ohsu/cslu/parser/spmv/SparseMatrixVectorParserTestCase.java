c261cbc6e762fb97eb2dba502216612cc55191af
Nate <devnull@localhost>
2011-01-10 18:49:38 +0000

    Beam-width prediction changes for ACL 2011

--- java/src/edu/ohsu/cslu/parser/spmv/SparseMatrixVectorParserTestCase.java
+++ java/src/edu/ohsu/cslu/parser/spmv/SparseMatrixVectorParserTestCase.java
@@ -14,12 +14,11 @@ import edu.ohsu.cslu.grammar.SparseMatrixGrammar.CartesianProductFunction;
 import edu.ohsu.cslu.grammar.SparseMatrixGrammar.LeftShiftFunction;
 import edu.ohsu.cslu.grammar.SparseMatrixGrammar.PerfectIntPairHashFilterFunction;
 import edu.ohsu.cslu.parser.ExhaustiveChartParserTestCase;
-import edu.ohsu.cslu.parser.cellselector.CellSelector;
-import edu.ohsu.cslu.parser.cellselector.CellSelector.CellSelectorType;
+import edu.ohsu.cslu.parser.cellselector.LeftRightBottomTopTraversal;
 import edu.ohsu.cslu.parser.chart.Chart;
+import edu.ohsu.cslu.parser.chart.ParallelArrayChart;
 import edu.ohsu.cslu.parser.chart.Chart.ChartCell;
 import edu.ohsu.cslu.parser.chart.Chart.ChartEdge;
-import edu.ohsu.cslu.parser.chart.ParallelArrayChart;
 import edu.ohsu.cslu.parser.spmv.SparseMatrixVectorParser.CartesianProductVector;
 
 /**
@@ -58,9 +57,8 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
 
         // Create the parser
         final SparseMatrixGrammar g = (SparseMatrixGrammar) simpleGrammar1;
-        final P p = createParser(g, CellSelector.create(CellSelectorType.LeftRightBottomTop), parserOptions(),
-                configProperties());
-        p.initParser(new int[4]);
+        final P p = createParser(g, new LeftRightBottomTopTraversal(), parserOptions(), configProperties());
+        p.initSentence(new int[4]);
         final Chart chart = p.chart;
 
         final int nn = g.mapNonterminal("NN");
@@ -113,8 +111,8 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
         final int[] expectedMidpoints = new int[] { 1, 1, 2, 3 };
 
         for (int i = 0; i < expectedChildren.length; i++) {
-            assertEquals("Wrong probability #" + i, expectedProbabilities[i],
-                    crossProductVector.probability(expectedChildren[i]), .01f);
+            assertEquals("Wrong probability #" + i, expectedProbabilities[i], crossProductVector
+                    .probability(expectedChildren[i]), .01f);
             assertEquals("Wrong midpoint #" + i, expectedMidpoints[i], crossProductVector.midpoint(expectedChildren[i]));
         }
     }
@@ -129,9 +127,8 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
 
         // Create the parser
         final SparseMatrixGrammar g = (SparseMatrixGrammar) simpleGrammar1;
-        final P p = createParser(g, CellSelector.create(CellSelectorType.LeftRightBottomTop), parserOptions(),
-                configProperties());
-        p.initParser(new int[4]);
+        final P p = createParser(g, new LeftRightBottomTopTraversal(), parserOptions(), configProperties());
+        p.initSentence(new int[4]);
         final Chart chart = p.chart;
 
         // Cell 0,3 contains NP -> NP NN (3/20)
@@ -190,14 +187,14 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
 
         // Create the parser
         final SparseMatrixGrammar g = (SparseMatrixGrammar) simpleGrammar1;
-        final SparseMatrixVectorParser<?, ?> p = createParser(g,
-                CellSelector.create(CellSelectorType.LeftRightBottomTop), parserOptions(), configProperties());
-        p.initParser(new int[4]);
+        final SparseMatrixVectorParser<?, ?> p = createParser(g, new LeftRightBottomTopTraversal(), parserOptions(),
+                configProperties());
+        p.initSentence(new int[4]);
 
         final ChartCell topCell = p.chart.getCell(0, 4);
         final int parent = g.mapNonterminal("NP");
-        topCell.updateInside(g.new Production(parent, g.mapNonterminal("NP"), g.mapNonterminal("NN"), -3.101f),
-                p.chart.getCell(0, 3), p.chart.getCell(3, 4), -3.101f);
+        topCell.updateInside(g.new Production(parent, g.mapNonterminal("NP"), g.mapNonterminal("NN"), -3.101f), p.chart
+                .getCell(0, 3), p.chart.getCell(3, 4), -3.101f);
 
         p.unarySpmv(topCell);
         assertEquals(2, topCell.getNumNTs());
@@ -220,7 +217,8 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
         final ChartCell cell_2_3 = chart.getCell(2, 3);
         cell_2_3.updateInside(simpleGrammar2.new Production("VP", "VB", -2.48491f, false), cell_2_3, null, -2.48491f);
         cell_2_3.updateInside(simpleGrammar2.new Production("NN", "market", -4.0547f, true), cell_2_3, null, -.40547f);
-        cell_2_3.updateInside(simpleGrammar2.new Production("VB", "market", -1.09861f, true), cell_2_3, null, -1.09861f);
+        cell_2_3
+                .updateInside(simpleGrammar2.new Production("VB", "market", -1.09861f, true), cell_2_3, null, -1.09861f);
 
         final ChartCell cell_3_4 = chart.getCell(3, 4);
         cell_3_4.updateInside(simpleGrammar2.new Production("VP", "VB", -2.07944f, false), cell_3_4, null, -2.07944f);
@@ -234,31 +232,42 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
 
         // Row of span 2
         final ChartCell cell_0_2 = chart.getCell(0, 2);
-        cell_0_2.updateInside(simpleGrammar2.new Production("NP", "DT", "NN", -1.38629f), cell_0_1, cell_1_2, -1.38629f);
-        cell_0_2.updateInside(simpleGrammar2.new Production("VP|VB", "NP", -1.38629f, false), cell_0_2, null, -1.38629f);
+        cell_0_2
+                .updateInside(simpleGrammar2.new Production("NP", "DT", "NN", -1.38629f), cell_0_1, cell_1_2, -1.38629f);
+        cell_0_2
+                .updateInside(simpleGrammar2.new Production("VP|VB", "NP", -1.38629f, false), cell_0_2, null, -1.38629f);
 
         final ChartCell cell_1_3 = chart.getCell(1, 3);
         cell_1_3.updateInside(simpleGrammar2.new Production("NP|NN", "NN", "NN", -.40547f), cell_1_2, cell_2_3,
                 -.40547f);
-        cell_1_3.updateInside(simpleGrammar2.new Production("NP", "NN", "NN", -2.19722f), cell_1_2, cell_2_3, -2.19722f);
-        cell_1_3.updateInside(simpleGrammar2.new Production("VP|VB", "NP", -2.19722f, false), cell_1_3, null, -2.19722f);
+        cell_1_3
+                .updateInside(simpleGrammar2.new Production("NP", "NN", "NN", -2.19722f), cell_1_2, cell_2_3, -2.19722f);
+        cell_1_3
+                .updateInside(simpleGrammar2.new Production("VP|VB", "NP", -2.19722f, false), cell_1_3, null, -2.19722f);
 
         final ChartCell cell_2_4 = chart.getCell(2, 4);
         cell_2_4.updateInside(simpleGrammar2.new Production("NP|NN", "NN", "NN", -1.09861f), cell_2_3, cell_3_4,
                 -1.09861f);
-        cell_2_4.updateInside(simpleGrammar2.new Production("NP", "NN", "NN", -2.89037f), cell_2_3, cell_3_4, -2.89037f);
-        cell_2_4.updateInside(simpleGrammar2.new Production("VP|VB", "NP", -2.89037f, false), cell_2_4, null, -2.89037f);
+        cell_2_4
+                .updateInside(simpleGrammar2.new Production("NP", "NN", "NN", -2.89037f), cell_2_3, cell_3_4, -2.89037f);
+        cell_2_4
+                .updateInside(simpleGrammar2.new Production("VP|VB", "NP", -2.89037f, false), cell_2_4, null, -2.89037f);
 
         final ChartCell cell_3_5 = chart.getCell(3, 5);
-        cell_3_5.updateInside(simpleGrammar2.new Production("VP", "VB", "RB", -1.79176f), cell_3_4, cell_4_5, -1.79176f);
-        cell_3_5.updateInside(simpleGrammar2.new Production("NP", "NN", "RB", -2.89037f), cell_3_4, cell_4_5, -2.89037f);
-        cell_3_5.updateInside(simpleGrammar2.new Production("VP|VB", "NP", -2.89037f, false), cell_3_5, null, -2.89037f);
+        cell_3_5
+                .updateInside(simpleGrammar2.new Production("VP", "VB", "RB", -1.79176f), cell_3_4, cell_4_5, -1.79176f);
+        cell_3_5
+                .updateInside(simpleGrammar2.new Production("NP", "NN", "RB", -2.89037f), cell_3_4, cell_4_5, -2.89037f);
+        cell_3_5
+                .updateInside(simpleGrammar2.new Production("VP|VB", "NP", -2.89037f, false), cell_3_5, null, -2.89037f);
 
         // Row of span 3
         final ChartCell cell_0_3 = chart.getCell(0, 3);
-        cell_0_3.updateInside(simpleGrammar2.new Production("NP", "DT", "NP", -3.58352f), cell_0_1, cell_1_3, -3.58352f);
+        cell_0_3
+                .updateInside(simpleGrammar2.new Production("NP", "DT", "NP", -3.58352f), cell_0_1, cell_1_3, -3.58352f);
         cell_0_3.updateInside(simpleGrammar2.new Production("S", "NP", "VP", -3.87120f), cell_0_2, cell_2_3, -3.87120f);
-        cell_0_3.updateInside(simpleGrammar2.new Production("VP|VB", "NP", -3.58352f, false), cell_0_3, null, -3.58352f);
+        cell_0_3
+                .updateInside(simpleGrammar2.new Production("VP|VB", "NP", -3.58352f, false), cell_0_3, null, -3.58352f);
         cell_0_3.updateInside(simpleGrammar2.new Production("TOP", "S", -3.87120f, false), cell_0_3, null, -3.87120f);
         cell_0_3.finalizeCell();
 
@@ -266,7 +275,8 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
         cell_1_4.updateInside(simpleGrammar2.new Production("NP", "NN", "NP|NN", -2.89037f), cell_1_2, cell_2_4,
                 -2.89037f);
         cell_1_4.updateInside(simpleGrammar2.new Production("S", "NP", "VP", -4.27667f), cell_1_3, cell_3_4, -4.27667f);
-        cell_1_4.updateInside(simpleGrammar2.new Production("VP|VB", "NP", -2.89037f, false), cell_1_4, null, -2.89037f);
+        cell_1_4
+                .updateInside(simpleGrammar2.new Production("VP|VB", "NP", -2.89037f, false), cell_1_4, null, -2.89037f);
         cell_1_4.updateInside(simpleGrammar2.new Production("TOP", "S", -4.27667f, false), cell_1_4, null, -4.27667f);
 
         final ChartCell cell_2_5 = chart.getCell(2, 5);
@@ -280,16 +290,17 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
     private void populateSimpleGrammar2Row4(final Chart chart) {
         // Row of span 4
         final ChartCell cell_0_4 = chart.getCell(0, 4);
-        cell_0_4.updateInside(simpleGrammar2.new Production("NP", "DT", "NP", -4.27667f), chart.getCell(0, 1),
-                chart.getCell(1, 4), -4.27667f);
-        cell_0_4.updateInside(simpleGrammar2.new Production("S", "NP", "VP", -5.66296f), chart.getCell(0, 3),
-                chart.getCell(3, 4), -5.66296f);
-        cell_0_4.updateInside(simpleGrammar2.new Production("VP|VB", "NP", -4.27667f, false), cell_0_4, null, -4.27667f);
+        cell_0_4.updateInside(simpleGrammar2.new Production("NP", "DT", "NP", -4.27667f), chart.getCell(0, 1), chart
+                .getCell(1, 4), -4.27667f);
+        cell_0_4.updateInside(simpleGrammar2.new Production("S", "NP", "VP", -5.66296f), chart.getCell(0, 3), chart
+                .getCell(3, 4), -5.66296f);
+        cell_0_4
+                .updateInside(simpleGrammar2.new Production("VP|VB", "NP", -4.27667f, false), cell_0_4, null, -4.27667f);
         cell_0_4.updateInside(simpleGrammar2.new Production("TOP", "S", -5.66296f, false), cell_0_4, null, -5.66296f);
 
         final ChartCell cell_1_5 = chart.getCell(1, 5);
-        cell_1_5.updateInside(simpleGrammar2.new Production("S", "NP", "VP", -3.98898f), chart.getCell(1, 3),
-                chart.getCell(3, 5), -3.98898f);
+        cell_1_5.updateInside(simpleGrammar2.new Production("S", "NP", "VP", -3.98898f), chart.getCell(1, 3), chart
+                .getCell(3, 5), -3.98898f);
         cell_1_5.updateInside(simpleGrammar2.new Production("TOP", "S", -3.98898f, false), cell_1_5, null, -3.98898f);
     }
 
@@ -304,9 +315,8 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
         final SparseMatrixGrammar g = (SparseMatrixGrammar) createGrammar(simpleGrammar2(), LeftShiftFunction.class);
 
         // Create the parser
-        final P p = createParser(g, CellSelector.create(CellSelectorType.LeftRightBottomTop), parserOptions(),
-                configProperties());
-        p.initParser(new int[5]);
+        final P p = createParser(g, new LeftRightBottomTopTraversal(), parserOptions(), configProperties());
+        p.initSentence(new int[5]);
         final Chart chart = p.chart;
 
         populateSimpleGrammar2Rows1_3(chart);
@@ -314,8 +324,8 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
 
         // Row of span 5
         final ChartCell cell_0_5 = chart.getCell(0, 5);
-        cell_0_5.updateInside(simpleGrammar2.new Production("S", "NP", "VP", -5.37528f), chart.getCell(0, 3),
-                chart.getCell(3, 5), -5.37528f);
+        cell_0_5.updateInside(simpleGrammar2.new Production("S", "NP", "VP", -5.37528f), chart.getCell(0, 3), chart
+                .getCell(3, 5), -5.37528f);
         cell_0_5.updateInside(simpleGrammar2.new Production("TOP", "S", -5.37528f, false), cell_0_5, null, -5.37528f);
 
         // Finalize all chart cells
@@ -330,8 +340,8 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
         assertEquals(21, crossProductVector.size());
 
         // Midpoint 1
-        assertEquals(-2.890f,
-                crossProductVector.probability(pack(g, g.mapNonterminal("DT"), g.mapNonterminal("VP|VB"))), .001f);
+        assertEquals(-2.890f, crossProductVector
+                .probability(pack(g, g.mapNonterminal("DT"), g.mapNonterminal("VP|VB"))), .001f);
         assertEquals(1, crossProductVector.midpoint(pack(g, g.mapNonterminal("DT"), g.mapNonterminal("VP|VB"))));
 
         assertEquals(-2.890f, crossProductVector.probability(pack(g, g.mapNonterminal("DT"), g.mapNonterminal("NP"))),
@@ -339,12 +349,12 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
         assertEquals(1, crossProductVector.midpoint(pack(g, g.mapNonterminal("DT"), g.mapNonterminal("NP"))));
 
         // Midpoint 2
-        assertEquals(-2.485f,
-                crossProductVector.probability(pack(g, g.mapNonterminal("NP"), g.mapNonterminal("NP|NN"))), .001f);
+        assertEquals(-2.485f, crossProductVector
+                .probability(pack(g, g.mapNonterminal("NP"), g.mapNonterminal("NP|NN"))), .001f);
         assertEquals(2, crossProductVector.midpoint(pack(g, g.mapNonterminal("NP"), g.mapNonterminal("NP|NN"))));
 
-        assertEquals(-4.277f,
-                crossProductVector.probability(pack(g, g.mapNonterminal("NP"), g.mapNonterminal("VP|VB"))), .001f);
+        assertEquals(-4.277f, crossProductVector
+                .probability(pack(g, g.mapNonterminal("NP"), g.mapNonterminal("VP|VB"))), .001f);
         assertEquals(2, crossProductVector.midpoint(pack(g, g.mapNonterminal("NP"), g.mapNonterminal("VP|VB"))));
 
         assertEquals(-4.277f, crossProductVector.probability(pack(g, g.mapNonterminal("NP"), g.mapNonterminal("NP"))),
@@ -373,8 +383,8 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
                 crossProductVector.probability(pack(g, g.mapNonterminal("NP"), g.mapNonterminal("VP"))), .001f);
         assertEquals(3, crossProductVector.midpoint(pack(g, g.mapNonterminal("NP"), g.mapNonterminal("VP"))));
 
-        assertEquals(-6.474f,
-                crossProductVector.probability(pack(g, g.mapNonterminal("NP"), g.mapNonterminal("VP|VB"))), .001f);
+        assertEquals(-6.474f, crossProductVector
+                .probability(pack(g, g.mapNonterminal("NP"), g.mapNonterminal("VP|VB"))), .001f);
         assertEquals(3, crossProductVector.midpoint(pack(g, g.mapNonterminal("NP"), g.mapNonterminal("VP|VB"))));
 
         assertEquals(-6.474f, crossProductVector.probability(pack(g, g.mapNonterminal("NP"), g.mapNonterminal("NP"))),
@@ -403,9 +413,8 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
                 PerfectIntPairHashFilterFunction.class);
 
         // Create the parser
-        final P p = createParser(g, CellSelector.create(CellSelectorType.LeftRightBottomTop), parserOptions(),
-                configProperties());
-        p.initParser(new int[5]);
+        final P p = createParser(g, new LeftRightBottomTopTraversal(), parserOptions(), configProperties());
+        p.initSentence(new int[5]);
         final Chart chart = p.chart;
 
         populateSimpleGrammar2Rows1_3(chart);
@@ -413,8 +422,8 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
 
         // Row of span 5
         final ChartCell cell_0_5 = chart.getCell(0, 5);
-        cell_0_5.updateInside(simpleGrammar2.new Production("S", "NP", "VP", -5.37528f), chart.getCell(0, 3),
-                chart.getCell(3, 5), -5.37528f);
+        cell_0_5.updateInside(simpleGrammar2.new Production("S", "NP", "VP", -5.37528f), chart.getCell(0, 3), chart
+                .getCell(3, 5), -5.37528f);
         cell_0_5.updateInside(simpleGrammar2.new Production("TOP", "S", -5.37528f, false), cell_0_5, null, -5.37528f);
 
         // Finalize all chart cells
@@ -459,9 +468,9 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
 
         // Create the parser
         final SparseMatrixGrammar g = (SparseMatrixGrammar) simpleGrammar2;
-        final SparseMatrixVectorParser<?, ?> p = createParser(g,
-                CellSelector.create(CellSelectorType.LeftRightBottomTop), parserOptions(), configProperties());
-        p.initParser(new int[5]);
+        final SparseMatrixVectorParser<?, ?> p = createParser(g, new LeftRightBottomTopTraversal(), parserOptions(),
+                configProperties());
+        p.initSentence(new int[5]);
         final Chart chart = p.chart;
 
         final float[] probabilities = new float[g.cartesianProductFunction().packedArraySize()];
@@ -569,9 +578,9 @@ public abstract class SparseMatrixVectorParserTestCase<P extends SparseMatrixVec
 
         // Create the parser
         final SparseMatrixGrammar g = (SparseMatrixGrammar) simpleGrammar2;
-        final SparseMatrixVectorParser<?, ?> p = createParser(g,
-                CellSelector.create(CellSelectorType.LeftRightBottomTop), parserOptions(), configProperties());
-        p.initParser(new int[5]);
+        final SparseMatrixVectorParser<?, ?> p = createParser(g, new LeftRightBottomTopTraversal(), parserOptions(),
+                configProperties());
+        p.initSentence(new int[5]);
 
         final Chart chart = p.chart;
         populateSimpleGrammar2Rows1_3(chart);
