ff3c1830e1c6adfe88a932404b5757d3be41daf6
Nate <devnull@localhost>
2011-01-10 18:42:12 +0000

    Beam-width prediction changes for ACL 2011

--- java/src/edu/ohsu/cslu/parser/ParserTrainer.java
+++ java/src/edu/ohsu/cslu/parser/ParserTrainer.java
@@ -2,49 +2,45 @@ package edu.ohsu.cslu.parser;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-
 
 import cltool4j.BaseCommandlineTool;
 import cltool4j.args4j.CmdLineParser;
 import cltool4j.args4j.Option;
 import edu.ohsu.cslu.grammar.Grammar;
-import edu.ohsu.cslu.grammar.Grammar.GrammarFormatType;
-import edu.ohsu.cslu.parser.Parser.ParserType;
 import edu.ohsu.cslu.parser.Parser.ResearchParserType;
 import edu.ohsu.cslu.parser.edgeselector.EdgeSelector;
 import edu.ohsu.cslu.parser.edgeselector.EdgeSelector.EdgeSelectorType;
+import edu.ohsu.cslu.perceptron.ModelTrainer;
 
 public class ParserTrainer extends BaseCommandlineTool {
 
+    // TODO: should combine this with ModelTrainer
+
     // == Parser options ==
-    @Option(name = "-p", aliases = { "--parser" }, metaVar = "parser", usage = "Parser implementation")
-    public ParserType parserType = ParserType.CKY;
+    // @Option(name = "-p", aliases = { "--parser" }, metaVar = "parser", usage = "Parser implementation")
+    // public ParserType parserType = ParserType.CKY;
 
-    @Option(name = "-rp", aliases = { "--research-parser" }, metaVar = "parser", usage = "Research Parser implementation")
-    private ResearchParserType researchParserType = null;
+    @Option(name = "-rp", metaVar = "parser", usage = "Research Parser implementation")
+    private ResearchParserType researchParserType = ResearchParserType.ECPCellCrossList;
 
-    @Option(name = "-fom", metaVar = "fom", usage = "Figure of Merit")
-    public EdgeSelectorType edgeFOMType = null;
+    @Option(name = "-g", required = true, metaVar = "grammar", usage = "Grammar file (text, gzipped text, or binary serialized")
+    private String grammarFile = null;
 
-    @Option(name = "-cellSelect", usage = "Train the specified Cell Selection model")
-    public boolean cellTrain = false;
+    // === Possible models to train ===
+    @Option(name = "-boundaryFOM", usage = "Train a Boundary Figure of Merit model")
+    public boolean boundaryFOM = false;
+    // public EdgeSelectorType edgeFOMType = null;
+
+    @Option(name = "-beamConf", usage = "Train Beam Confidence model")
+    public boolean beamConf = false;
 
     @Option(name = "-cellConstraints", usage = "Train a Cell Constraints model")
     public boolean cellConstraints = false;
 
-    // == Grammar options ==
-    @Option(name = "-g", required = true, metaVar = "grammar", usage = "Grammar file (text, gzipped text, or binary serialized")
-    private String grammarFile = null;
-
-    @Option(name = "-gf", aliases = { "--grammar-format" }, metaVar = "format", usage = "Format of grammar file")
-    public GrammarFormatType grammarFormat = GrammarFormatType.CSLU;
-
     public BufferedWriter outputStream = new BufferedWriter(new OutputStreamWriter(System.out));
     public BufferedReader inputStream = new BufferedReader(new InputStreamReader(System.in));
-
     private Grammar grammar;
 
     public static void main(final String[] args) throws Exception {
@@ -54,28 +50,21 @@ public class ParserTrainer extends BaseCommandlineTool {
     @Override
     public void setup(final CmdLineParser cmdlineParser) throws Exception {
 
-        // Handle prefixes with or without trailing periods.
-        String grammarFileName = grammarFile + (grammarFile.endsWith(".") ? "" : ".") + "pcfg";
-
-        // Handle gzipped grammar files
-        if (!new File(grammarFileName).exists() && new File(grammarFileName + ".gz").exists()) {
-            grammarFileName = grammarFileName + ".gz";
-        }
-
-        // Read in the grammar
-        grammar = ParserDriver.readGrammar(grammarFileName, researchParserType, null);
+        grammar = ParserDriver.readGrammar(grammarFile, researchParserType, null);
     }
 
     @Override
     public void run() throws Exception {
 
-        if (edgeFOMType != null) {
+        if (boundaryFOM == true) {
             // To train a BoundaryInOut FOM model we need a grammar and
             // binarized gold input trees with NTs from same grammar
-            final EdgeSelector edgeSelector = EdgeSelector.create(edgeFOMType, grammar, null);
+            final EdgeSelector edgeSelector = EdgeSelector.create(EdgeSelectorType.BoundaryInOut, grammar, null);
             edgeSelector.train(inputStream);
             edgeSelector.writeModel(outputStream);
-        } else if (cellTrain == true) {
+        } else if (beamConf == true) {
+            final ModelTrainer m = new ModelTrainer();
+            m.natesTraining();
             // final PerceptronCellSelector perceptronCellSelector = (PerceptronCellSelector)
             // CellSelector.create(cellSelectorType, cellModelStream, cslutScoresStream);
             // final BSCPPerceptronCellTrainer parser = new BSCPPerceptronCellTrainer(opts, (LeftHashGrammar) grammar);
--- java/src/edu/ohsu/cslu/parser/ParserUtil.java
+++ java/src/edu/ohsu/cslu/parser/ParserUtil.java
@@ -3,6 +3,7 @@ package edu.ohsu.cslu.parser;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.StringTokenizer;
@@ -60,5 +61,53 @@ public class ParserUtil {
             is = new GZIPInputStream(is);
         }
         return is;
+    }
+
+    public static String intArray2Str(final int[] data) {
+        String result = "";
+        for (final int val : data) {
+            result += val + " ";
+        }
+        return result.trim();
+    }
+
+    public static int bool2int(final boolean value) {
+        if (value) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static int[] strToIntArray(final String str) {
+        return strToIntArray(str, ",", 0, -1);
+    }
+
+    public static int[] strToIntArray(final String str, final String delim, final float defaultVal, final int numBins) {
+        final float[] floatArray = strToFloatArray(str, delim, defaultVal, numBins);
+        final int[] intArray = new int[floatArray.length];
+        for (int i = 0; i < floatArray.length; i++) {
+            intArray[i] = (int) floatArray[i];
+        }
+        return intArray;
+    }
+
+    public static float[] strToFloatArray(final String str) {
+        return strToFloatArray(str, ",", 0f, -1);
+    }
+
+    // assuming str has the form: x,y,z
+    public static float[] strToFloatArray(final String str, final String delim, final float defaultVal, int numBins) {
+        final String[] tokens = str.split(delim);
+        if (numBins == -1) {
+            numBins = tokens.length;
+        }
+
+        final float[] array = new float[tokens.length];
+        Arrays.fill(array, defaultVal);
+
+        for (int i = 0; i < tokens.length; i++) {
+            array[i] = Float.parseFloat(tokens[i]);
+        }
+        return array;
     }
 }
