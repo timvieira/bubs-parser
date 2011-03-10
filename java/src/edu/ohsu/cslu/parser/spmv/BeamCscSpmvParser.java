df7fe305201c1182d5ad674834e87e44bbf4379a
dunlopa <devnull@localhost>
2011-03-10 19:36:18 +0000

    Renamed beam prediction method in CellSelector hierarchy to getBeamWidth(). We're agreed that it's probably still not modeled right, but this is an improvement.

--- java/src/edu/ohsu/cslu/parser/spmv/BeamCscSpmvParser.java
+++ java/src/edu/ohsu/cslu/parser/spmv/BeamCscSpmvParser.java
@@ -86,7 +86,7 @@ public class BeamCscSpmvParser extends CscSpmvParser {
 
         // Push all binary or lexical edges onto a bounded priority queue
         final int cellBeamWidth = (end - start == 1 ? lexicalRowBeamWidth : Math.min(
-                cellSelector.getCellValue(start, end), beamWidth));
+                cellSelector.getBeamWidth(start, end), beamWidth));
         final BoundedPriorityQueue q = new BoundedPriorityQueue(cellBeamWidth, grammar);
 
         final float[] tmpFoms = new float[grammar.numNonTerms()];
