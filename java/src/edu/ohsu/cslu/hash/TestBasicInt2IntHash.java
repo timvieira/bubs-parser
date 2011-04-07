dd648cf5c0f4002c3f8ef8b6ac4173b0031a30f3
dunlopa <devnull@localhost>
2011-04-07 22:28:06 +0000

    Removed edu.ohsu.cslu.hash package, which is now in cslu-common from bubs-parser project.

--- java/src/edu/ohsu/cslu/hash/TestBasicInt2IntHash.java
+++
@@ -1,9 +0,0 @@
-package edu.ohsu.cslu.hash;
-
-public class TestBasicInt2IntHash extends ImmutableInt2IntHashTestCase {
-
-    @Override
-    protected ImmutableInt2IntHash hash(final int[] keys, final int modulus) {
-        return new BasicInt2IntHash(keys);
-    }
-}
