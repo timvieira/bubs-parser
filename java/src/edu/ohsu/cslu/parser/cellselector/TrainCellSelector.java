33e1d0638a58c457d75dfeba97a8fb9472fd0289
bodenstab <devnull@localhost>
2011-08-05 09:25:28 -0700

    adding stub file for TrainCellSelector

---
+++ java/src/edu/ohsu/cslu/parser/cellselector/TrainCellSelector.java
@@ -0,0 +1,46 @@
+package edu.ohsu.cslu.parser.cellselector;
+
+import cltool4j.BaseCommandlineTool;
+
+public class TrainCellSelector extends BaseCommandlineTool {
+
+    @Override
+    protected void run() throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+    /*
+     * // == Parser options == // @Option(name = "-p", aliases = { "--parser" }, metaVar = "parser", usage =
+     * "Parser implementation") // public ParserType parserType = ParserType.CKY;
+     * 
+     * // @Option(name = "-rp", metaVar = "parser", usage = "Research Parser implementation") private ResearchParserType
+     * researchParserType = ResearchParserType.ECPCellCrossList;
+     * 
+     * @Option(name = "-g", required = true, metaVar = "grammar", usage =
+     * "Grammar file (text, gzipped text, or binary serialized") private String grammarFile = null;
+     * 
+     * // @Option(name = "-boundaryFOM", usage = "Train a Boundary Figure of Merit model") // public boolean boundaryFOM
+     * = false; // public EdgeSelectorType edgeFOMType = null;
+     * 
+     * // @Option(name = "-beamConf", usage = "Train Beam Confidence model") // public boolean beamConf = false;
+     * 
+     * // @Option(name = "-cellConstraints", usage = "Train a Cell Constraints model") // public boolean cellConstraints
+     * = false;
+     * 
+     * public BufferedWriter outputStream = new BufferedWriter(new OutputStreamWriter(System.out)); public
+     * BufferedReader inputStream = new BufferedReader(new InputStreamReader(System.in)); private Grammar grammar;
+     * 
+     * public static void main(final String[] args) { run(args); }
+     * 
+     * @Override public void run() throws Exception { // To train a BoundaryInOut FOM model we need a grammar and //
+     * binarized gold input trees with NTs from same grammar grammar = ParserDriver.readGrammar(grammarFile,
+     * researchParserType, null);
+     * 
+     * // } else if (beamConf == true) { // final ModelTrainer m = new ModelTrainer(); // m.natesTraining(); // // final
+     * PerceptronCellSelector perceptronCellSelector = (PerceptronCellSelector) // //
+     * CellSelector.create(cellSelectorType, cellModelStream, cslutScoresStream); // // final BSCPPerceptronCellTrainer
+     * parser = new BSCPPerceptronCellTrainer(opts, (LeftHashGrammar) // // grammar); // //
+     * perceptronCellSelector.train(inputStream, parser); // } else { // System.out.println("ERROR."); // } }
+     */
+
+}
