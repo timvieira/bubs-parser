b6b4872cf890c85f17ba8e3cd8ad44d0ad9f2cf8
AaronD <devnull@localhost>
2011-11-10 15:27:00 -0800

    Added some Java assertions on Probability (these will only fire when debugging with the -ea flag, so they won't consume CPU at runtime).

--- java/src/edu/ohsu/cslu/grammar/Production.java
+++ java/src/edu/ohsu/cslu/grammar/Production.java
@@ -87,7 +87,12 @@ public class Production implements Serializable, Cloneable {
     // Unary or lexical production
     public Production(final int parent, final int child, final float prob, final boolean isLex,
             final SymbolSet<String> vocabulary, final SymbolSet<String> lexicon) {
+
         assert parent != -1 && child != -1;
+        assert !Float.isInfinite(prob);
+        assert !Float.isNaN(prob);
+        assert prob <= 0f;
+
         this.parent = parent;
         this.leftChild = child;
         this.vocabulary = vocabulary;
