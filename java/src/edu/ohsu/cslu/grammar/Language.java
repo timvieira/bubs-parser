42cf99ad673f891c08aaa0d793a7a6a6912e18ce
AaronD <devnull@localhost>
2011-11-28 06:37:38 -0800

    Added Language enum, representing language or genre (i.e., we'll probably add new 'Languages' for non-canonical text genres even in English.

---
+++ java/src/edu/ohsu/cslu/grammar/Language.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2010, 2011 Aaron Dunlop and Nathan Bodenstab
+ * 
+ * This file is part of the BUBS Parser.
+ * 
+ * The BUBS Parser is free software: you can redistribute it and/or 
+ * modify  it under the terms of the GNU Affero General Public License 
+ * as published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version.
+ * 
+ * The BUBS Parser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the BUBS Parser. If not, see <http://www.gnu.org/licenses/>
+ */
+package edu.ohsu.cslu.grammar;
+
+/**
+ * Represents language or genre.
+ * 
+ * @author Aaron Dunlop
+ */
+public enum Language {
+    /** Edited english; e.g. WSJ text */
+    English,
+
+    /** Edited Chinese; e.g. Chinese treebank text */
+    Chinese;
+}
