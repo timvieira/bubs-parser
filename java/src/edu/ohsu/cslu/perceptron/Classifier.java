def59953d60f015be9633300bdb08a2f09ea4cd5
AaronD <devnull@localhost>
2012-04-27 15:55:58 -0700

    Minor updates to Classifier / Perceptron hierarchy to enable Java serialization and simplify use outside of beam-confidence modeling.

--- java/src/edu/ohsu/cslu/perceptron/Classifier.java
+++ java/src/edu/ohsu/cslu/perceptron/Classifier.java
@@ -20,11 +20,14 @@ package edu.ohsu.cslu.perceptron;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.Serializable;
 
 import edu.ohsu.cslu.datastructs.vectors.SparseBitVector;
 import edu.ohsu.cslu.datastructs.vectors.Vector;
 
-public abstract class Classifier {
+public abstract class Classifier implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     protected int bins[];
 
