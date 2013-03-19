e9a35a67bf6825aa12e2cedbd7e23f379c0ff0b3
AaronD <aaron.dunlop@gmail.com>
2013-03-18 21:59:41 -0700

    A little cleanup and formatting.

--- java/berkeley-src/edu/berkeley/nlp/util/ScalingTools.java
+++ java/berkeley-src/edu/berkeley/nlp/util/ScalingTools.java
@@ -3,75 +3,93 @@
  */
 package edu.berkeley.nlp.util;
 
-
 /**
- * @author petrov
+ * Provides a scaling system, to avoid numeric overflow and underflow with IEEE 64-bit double precision floating-point
+ * numbers (limited to ~e^709).
  * 
+ * @author petrov
  */
 public class ScalingTools {
-    // SCALING
-    public static final int LOGSCALE = 100;
-    public static final double SCALE = Math.exp(LOGSCALE);
 
-    // Note: e^709 is the largest double java can handle.
+    private static final int LOGSCALE = 100;
+    public static final double SCALE = Math.exp(LOGSCALE);
 
     public static double calcScaleFactor(final double logScale) {
         return calcScaleFactor(logScale, SCALE);
     }
 
     public static double calcScaleFactor(final double logScale, final double scale) {
+
         if (logScale == Integer.MIN_VALUE) {
-            return 0.0;// System.out.println("give me a break!");
+            return 0.0;
         }
-        if (logScale == 0.0)
+
+        if (logScale == 0.0) {
             return 1.0;
-        if (logScale == 1.0)
+        }
+
+        if (logScale == 1.0) {
             return scale;
-        if (logScale == 2.0)
+        }
+
+        if (logScale == 2.0) {
             return scale * scale;
-        if (logScale == 3.0)
+        }
+
+        if (logScale == 3.0) {
             return scale * scale * scale;
-        if (logScale == -1.0)
+        }
+
+        if (logScale == -1.0) {
             return 1.0 / scale;
-        if (logScale == -2.0)
+        }
+
+        if (logScale == -2.0) {
             return 1.0 / scale / scale;
-        if (logScale == -3.0)
+        }
+
+        if (logScale == -3.0) {
             return 1.0 / scale / scale / scale;
+        }
+
         return Math.pow(scale, logScale);
     }
 
     public static int scaleArray(final double[] scores, final int previousScale) {
+
         if (previousScale == Integer.MIN_VALUE) {
-            return previousScale;// System.out.println("give me a break!");
+            return previousScale;
         }
-        // if (true) return previousScale;
+
         int logScale = 0;
         double scale = 1.0;
         double max = ArrayUtil.max(scores);
         if (max == Double.POSITIVE_INFINITY) {
-            // System.out.println("Infinity");
             return 0;
         }
-        if (max == 0)
+
+        if (max == 0) {
             return previousScale;
+        }
+
         while (max > SCALE) {
             max /= SCALE;
             scale *= SCALE;
             logScale += 1;
         }
+
         while (max > 0.0 && max < 1.0 / SCALE) {
             max *= SCALE;
             scale /= SCALE;
             logScale -= 1;
         }
+
         if (logScale != 0) {
             for (int i = 0; i < scores.length; i++) {
                 scores[i] /= scale;
             }
         }
-        // if (SloppyMath.isDangerous(ArrayMath.max(scores))){
-        // System.out.println("Undeflow when scaling scores!");
-        // }
+
         return previousScale + logScale;
     }
 }
